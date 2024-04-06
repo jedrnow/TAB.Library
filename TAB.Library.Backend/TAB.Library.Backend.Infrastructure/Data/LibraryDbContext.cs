1adcf1f188c201dfc37b3d0b77ba83e24af2e368
Jędrzej Nowaczyk <56341706+jedrnow@users.noreply.github.com>
2024-04-06 21:57:26 +0200

    #4: Podłączenie bazy danych. (#16)

---
+++ TAB.Library.Backend/TAB.Library.Backend.Infrastructure/Data/LibraryDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TAB.Library.Backend.Infrastructure.Data
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync() > 0;
+        }
+    }
+}
