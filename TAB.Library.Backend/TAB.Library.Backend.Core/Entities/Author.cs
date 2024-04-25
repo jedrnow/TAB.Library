0fb7f0f627a358ae82c6f7671981b3128a4043d1
Jędrzej Nowaczyk <56341706+jedrnow@users.noreply.github.com>
2024-04-25 17:27:26 +0200

    #12: Dodanie tabeli "Authors". (#25)

---
+++ TAB.Library.Backend/TAB.Library.Backend.Core/Entities/Author.cs
@@ -0,0 +1,8 @@
+﻿namespace TAB.Library.Backend.Core.Entities
+{
+    public class Author : BaseEntity
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
---
+++ TAB.Library.Backend/TAB.Library.Backend.Infrastructure/Configurations/AuthorConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TAB.Library.Backend.Core.Entities;
+
+namespace TAB.Library.Backend.Infrastructure.Configurations
+{
+    public class AuthorConfiguration : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.HasData(
+                new Author { Id = 1, FirstName = "Andrzej", LastName = "Sapkowski", CreatedAtUtc = new DateTime(2024, 4, 25), UpdatedAtUtc = new DateTime(2024, 4, 25) },
+                new Author { Id = 2, FirstName = "Brian", LastName = "Herbert", CreatedAtUtc = new DateTime(2024, 4, 25), UpdatedAtUtc = new DateTime(2024, 4, 25) },
+                new Author { Id = 3, FirstName = "Remigiusz", LastName = "Mróz", CreatedAtUtc = new DateTime(2024, 4, 25), UpdatedAtUtc = new DateTime(2024, 4, 25) },
+                new Author { Id = 4, FirstName = "Riley", LastName = "Sager", CreatedAtUtc = new DateTime(2024, 4, 25), UpdatedAtUtc = new DateTime(2024, 4, 25) },
+                new Author { Id = 5, FirstName = "Vance", LastName = "Ashlee", CreatedAtUtc = new DateTime(2024, 4, 25), UpdatedAtUtc = new DateTime(2024, 4, 25) },
+                new Author { Id = 6, FirstName = "Dougal", LastName = "Dixon", CreatedAtUtc = new DateTime(2024, 4, 25), UpdatedAtUtc = new DateTime(2024, 4, 25) },
+                new Author { Id = 7, FirstName = "Antoni", LastName = "Dudek", CreatedAtUtc = new DateTime(2024, 4, 25), UpdatedAtUtc = new DateTime(2024, 4, 25) },
+                new Author { Id = 8, FirstName = "David", LastName = "Goggins", CreatedAtUtc = new DateTime(2024, 4, 25), UpdatedAtUtc = new DateTime(2024, 4, 25) },
+                new Author { Id = 9, FirstName = "Marek", LastName = "Wnukowski", CreatedAtUtc = new DateTime(2024, 4, 25), UpdatedAtUtc = new DateTime(2024, 4, 25) },
+                new Author { Id = 10, FirstName = "Carl", LastName = "Barks", CreatedAtUtc = new DateTime(2024, 4, 25), UpdatedAtUtc = new DateTime(2024, 4, 25) },
+                new Author { Id = 11, FirstName = "Stephen", LastName = "King", CreatedAtUtc = new DateTime(2024, 4, 25), UpdatedAtUtc = new DateTime(2024, 4, 25) },
+                new Author { Id = 12, FirstName = "Stephen", LastName = "Hawking", CreatedAtUtc = new DateTime(2024, 4, 25), UpdatedAtUtc = new DateTime(2024, 4, 25) }
+            );
+        }
+    }
+}
