a3e1482bb008d618fe832b3a0e05d7f138235377
jedrnow <jedrzej.nowaczyk00@gmail.com>
2024-06-11 10:27:04 +0200

    #31: Usuwanie książek - backend

---
+++ TAB.Library.Backend/TAB.Library.Backend.Application/Handlers/DeleteBookCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using TAB.Library.Backend.Application.Commands;
+using TAB.Library.Backend.Core.Exceptions;
+using TAB.Library.Backend.Infrastructure.Services.Abstractions;
+
+namespace TAB.Library.Backend.Application.Handlers
+{
+    public class DeleteBookCommandHandler : IRequestHandler<DeleteBookCommand, bool>
+    {
+        private readonly IUserService _userService;
+        private readonly IBookService _bookService;
+
+        public DeleteBookCommandHandler(IUserService userService, IBookService bookService)
+        {
+            _userService = userService;
+            _bookService = bookService;
+        }
+
+        public async Task<bool> Handle(DeleteBookCommand request, CancellationToken cancellationToken)
+        {
+            bool userHasAdminPermissions = await _userService.CheckAdminPermissions(request.Username);
+            if (!userHasAdminPermissions) throw new UserUnauthorizedException();
+
+            bool bookDeleted = await _bookService.DeleteBook(request.BookId);
+
+            return bookDeleted;
+        }
+    }
+}
--- TAB.Library.Backend/TAB.Library.Backend.Infrastructure/Services/Abstractions/IBookService.cs
+++ TAB.Library.Backend/TAB.Library.Backend.Infrastructure/Services/Abstractions/IBookService.cs
@@ -7,6 +7,7 @@ namespace TAB.Library.Backend.Infrastructure.Services.Abstractions
         Task<bool> CheckIfBookExists(int bookId);
         Task<int> CreateBook(string title, int publishYear, int authorId, int categoryId);
         Task<bool> UpdateBook(int bookId, string title, int publishYear, int authorId, int categoryId);
+        Task<bool> DeleteBook(int bookId);
         Task<BookDTO> GetBookById(int bookId);
         Task<PaginatedListDTO<BookDTO>> GetPaginatedBookList(int pageNumber, int pageSize);
     }
